a59c379884a2a359cb1b1e2cdee77b2b1ee9bee4
jgabryelp <joaogabryelcaldeirap@gmail.com>
2026-07-31 02:56:54 -0300

    graficos completos

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #importando as bibliotecas
 import gdown 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
@@ -30,13 +31,13 @@ def formatarparadolar(valor):
 categoriaseseusfaturamentos = basededados[["Category", "Amount"]].groupby("Category").sum() #selecionando as colunas de categoria dos produtos e valor das vendas de cada categoria 
 categoriaseseusfaturamentos = categoriaseseusfaturamentos.reset_index().sort_values("Amount", ascending=False) #tirando o index da tabela e ordenando os faturamentos do maior para o menor
 categoriaseseusfaturamentos["Amount"] = categoriaseseusfaturamentos["Amount"].apply(formatarparadolar) #aplicando a formatação para dólar
-print(categoriaseseusfaturamentos)
+#print(categoriaseseusfaturamentos)
 
 
 cidadescommaispedidos = basededados["ship-city"].value_counts() #selecionando o nome das cidades e quantas vezes elas aparecem nas pedidos
 cidadescommaispedidos = cidadescommaispedidos.reset_index() #transformando o value counts em uma tabela comum
 cidadescommaispedidos = cidadescommaispedidos[:15] #selecionando as 15 cidades com maior quantidade de pedidos
-print(cidadescommaispedidos)
+#print(cidadescommaispedidos)
 
 
 evoluçaofaturamentomeses = basededados[["Date", "Amount"]] #selecionando a coluna de datas dos pedidos e valor dos pedidos
@@ -64,24 +65,24 @@ nomesmeses = {
 
 evoluçaofaturamentomeses["Month"] = evoluçaofaturamentomeses["Month"].map(nomesmeses) #aplicando os nomes dos meses na ordem do calendário
 
-evoluçaofaturamentomeses["Amount"] = evoluçaofaturamentomeses["Amount"].apply(formatarparadolar) #formatando o faturamento para dólar
+#evoluçaofaturamentomeses["Amount"] = evoluçaofaturamentomeses["Amount"].apply(formatarparadolar) #formatando o faturamento para dólar
 
-print(evoluçaofaturamentomeses)
+#print(evoluçaofaturamentomeses)
 
 
 
 #criando o relatório da análise
-cidadescommaispedidosrelatorio = ""
-for indice, linha in cidadescommaispedidos.iterrows(): 
-    cidadescommaispedidosrelatorio += (f"  {linha["ship-city"]} -"  f" {linha["count"]} Pedidos\n")
-                                                    
 categoriaseseusfaturamentosrelatorio = ""                       #formatando as tabelas que serão escritas no relatório
 for indice, linha in categoriaseseusfaturamentos.iterrows():
     categoriaseseusfaturamentosrelatorio += (f"  {linha["Category"]} -"  f" Faturou: {linha["Amount"]}\n") 
 
+cidadescommaispedidosrelatorio = ""
+for indice, linha in cidadescommaispedidos.iterrows(): 
+    cidadescommaispedidosrelatorio += (f"  {linha["ship-city"]} -"  f" {linha["count"]} Pedidos\n")
+                                                    
 evoluçaofaturamentomesesrelatorio = ""
 for indice, linha in evoluçaofaturamentomeses.iterrows():
-    evoluçaofaturamentomesesrelatorio += (f"  {linha["Month"]} -"  f" {linha["Amount"]}\n")    
+    evoluçaofaturamentomesesrelatorio += (f"  {linha["Month"]} -"  f" ${linha["Amount"]:,.2f}\n")    
 
 
 relatorio = f"""
@@ -102,5 +103,41 @@ FATURAMENTO AO LONGO DOS MESES:
 {evoluçaofaturamentomesesrelatorio}
 
 """
-
 print(relatorio)
+
+
+
+#criando os gráficos
+plt.figure(figsize=(11, 5))
+
+plt.bar(categoriaseseusfaturamentos["Category"], categoriaseseusfaturamentos["Amount"], color="blue")
+
+plt.title("Categorias de produtos e seus Faturamentos")
+plt.xlabel("Categorias")
+plt.ylabel("Faturamento em Dólar")
+
+plt.show()
+
+
+plt.figure(figsize=(12, 7))
+plt.bar(cidadescommaispedidos["ship-city"], cidadescommaispedidos["count"], color="steelblue")
+
+plt.title("As 15 Cidades com maior quantidade de Pedidos")
+plt.xlabel("Cidades")
+plt.ylabel("Quantidade de Pedidos")
+
+plt.tick_params(axis="x", rotation=90)
+
+plt.show()
+
+
+plt.figure(figsize=(11, 5))
+plt.plot(evoluçaofaturamentomeses["Month"], evoluçaofaturamentomeses["Amount"], marker="o")
+
+plt.title("Faturamento total ao longo dos Meses")
+plt.ylabel("Faturamento em Dólar")
+plt.xlabel("Meses")
+
+plt.yticks(evoluçaofaturamentomeses["Amount"], evoluçaofaturamentomeses["Amount"].apply(formatarparadolar))
+
+plt.show()
